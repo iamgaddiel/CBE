db8c20d5e1412a85ccccfade47229b52bc0db35f
iamgaddiel <ighotagaddiel98@gmail.com>
2022-12-13 21:52:38 -0500

    updated department code

--- src/hooks/useDepartment.jsx
+++ src/hooks/useDepartment.jsx
@@ -1,27 +1,85 @@
 import React from 'react'
+import { userAtom, userTokenAtom } from '../../recoilStates';
 
 const useDepartment = () => {
-    const createDepartment = async (name, facultyId, code) => {
+    const userToken = useRecoilValue(userTokenAtom)
+
+
+    const createDepartment = async (name, faculty, code) => {
         try {
             const url = `${baseUrl}/department/create`;
-            const res = await axios.post(url, { name, facultyId, code })
-            return res;
+            const { data } = await axios.post(url, {
+                data: {
+                    name,
+                    faculty,
+                    code
+                },
+                headers: {
+                    Authorization: `Bearer ${userToken}`
+                }
+            })
+            return data;
         }
         catch (err) {
-            throw new Error(err)
+            throw new Error(err.response.data)
         }
     }
 
-    const updateDependent = async (
-        
-    ) => { }
+    const updateDependent = async (name, faculty, code) => {
+        try {
+            const url = `${baseUrl}/department/create`;
+            const { data } = await axios.put(url, {
+                data: {
+                    name,
+                    faculty,
+                    code
+                },
+                headers: {
+                    Authorization: `Bearer ${userToken}`
+                }
+            })
+            return data;
+        }
+        catch (err) {
+            throw new Error(err.response.data)
+        }
+    }
 
-    const deleteDependent = async () => { }
+    const deleteDependent = async (id) => {
+        try {
+            const url = `${baseUrl}/department/delete/${id}`;
+            const { data } = await axios.put(url, {
+                headers: {
+                    Authorization: `Bearer ${userToken}`
+                }
+            })
+            return data;
+        }
+        catch (err) {
+            throw new Error(err.response.data)
+        }
+    }
 
-    const getDependent = async () => { }
+    const getDependent = async (facultyId) => { 
+        try {
+            const url = `${baseUrl}/department/all?faculty=${facultyId}`;
+            const { data } = await axios.put(url, {
+                headers: {
+                    Authorization: `Bearer ${userToken}`
+                }
+            })
+            return data;
+        }
+        catch (err) {
+            throw new Error(err.response.data)
+        }
+    }
 
     return {
-
+        createDepartment,
+        updateDependent,
+        deleteDependent,
+        getDependent
     }
 }
 
